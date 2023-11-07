a13ad5f667afb8687352551d74cdc0009e427da8
sxgmytt <146790661+sxgmytt@users.noreply.github.com>
2023-11-07 10:59:00 +0900

    Add files via upload

--- 2023_fall/lec04/homework4.py
+++ 2023_fall/lec04/homework4.py
@@ -1,10 +1,10 @@
 
 def list_to_dict(input_list):
-    '''
-    This function should return a dictionary in which each element of 
-    `input_list` is a value, and the corresponding key is the numerical 
-    index of that element in `input_list`. 
-    '''
+    result_dict = {}
+    for index, value in enumerate(input_list):
+        result_dict[index] = value
+    return result_dict
+
     pass
 
 
